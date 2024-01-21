4eaa00b68d74cafe815facea2e674d0bc1362c61
Deepanshu Sharma <deepanshus094@gmail.com>
2024-01-21 19:01:15 +0530

    oAuth2 to send otp

---
+++ src/utility/config.js
@@ -0,0 +1,8 @@
+export default {
+  clientId:
+    "679993717756-5udrjupv2ieca9dvivucflo0aa0ahivr.apps.googleusercontent.com",
+  clientSecret: "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",
+  refreshToken:
+    "1//04q4gSQm8ZJvaCgYIARAAGAQSNwF-L9Ir6jszY4eKMquzaKAAaRWdvUedFL8gG0eAVPcInSK-VPTwfL8fnD4HbvFpFlwO6llHf0Q",
+  redirectUri: "https://developers.google.com/oauthplayground",
+};
--- src/utility/sendMail.js
+++ src/utility/sendMail.js
@@ -1,19 +1,42 @@
 import nodemailer from "nodemailer";
+import { OAuth2Client } from "google-auth-library";
+import config from "./config.js";
+const oauth2Client = new OAuth2Client(
+  config.clientId,
+  config.clientSecret,
+  config.redirectUri
+);
 
-const sendMail = (subject, message, targetEmail) => {
+oauth2Client.setCredentials({
+  refresh_token: config.refreshToken,
+});
+
+const sendMail = async (subject, message, targetEmail) => {
+  console.log("token generating");
+  const accessToken = await oauth2Client.getAccessToken();
+  console.log("token generated");
   var transporter = nodemailer.createTransport({
+    // service: "gmail",
+    // host: "smtp.gmail.com",
+    // port: 465,
+    // secure: true,
+    // auth: {
+    //   user: "bhavusha590@gmail.com",
+    //   pass: "qesmjqvbryyvvztp",
+    // },
     service: "gmail",
-    host: "smtp.gmail.com",
-    port: 465,
-    secure: true,
     auth: {
-      user: "bhavusha590@gmail.com",
-      pass: "nrsrmlmnknzzzjwb",
+      type: "OAuth2",
+      user: "foevideosinfo@gmail.com",
+      clientId: config.clientId,
+      clientSecret: config.clientSecret,
+      refreshToken: config.refreshToken,
+      accessToken,
     },
   });
 
   var mailOptions = {
-    from: "bhavusha590@gmail.com",
+    from: "foevideosinfo@gmail.com",
     to: targetEmail,
     subject: subject,
 
